39874e2ccfe5d72a5f6f41fed4bb86fe0471ecfa
sravanichinthalapudi36 <68219520+sravanichinthalapudi36@users.noreply.github.com>
2020-08-03 18:19:30 +0530

    Add files via upload

---
+++ AbstractClasses.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace consoleFirstApp
+{
+    abstract class Quadralateral
+    {
+        public int length;
+        public Quadralateral(int _length)
+        {
+            length = _length;
+        }
+        public void Dispaly()
+        {
+            Console.WriteLine("jbkk");
+        }
+        public abstract int Area();
+        
+    }
+    class Square : Quadralateral
+    {
+        public Square(int _len) : base(_len)
+        {
+
+        }
+        public override int Area()
+        {
+            int r = length * length;
+            return r;
+        }
+    }
+    class Rectangle : Quadralateral
+    {
+        int breadth;
+        public Rectangle(int _breadth,int _length) : base(_length)
+        {
+            breadth = _breadth;
+        }
+        public override int Area()
+        {
+            int t = breadth * length;
+            return t;
+        }
+
+    }
+    class Que1
+    {
+        static void Main()
+        {
+            Square sq = new Square(4);
+            Console.WriteLine("square area is:"+sq.Area());
+            Quadralateral q = new Rectangle(4, 5);
+            Console.WriteLine("rectangle area is:"+q.Area());
+
+            if (sq.Area() > q.Area())
+            {
+                Console.WriteLine("Square area is greater than Rectangle area");
+            }
+            else
+            {
+                Console.WriteLine("Rectancle area is greater than square area");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ InheritanceASS.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleFirstApp
+{
+    class Game
+    {
+        public static int noOfPlayers;
+        public static string country;
+        public Game()
+        {
+
+        }
+        public Game(int _noOfPlayers, String _country)
+        {
+            noOfPlayers = _noOfPlayers;
+            country = _country;
+
+        }
+        public void Display()
+        {
+            Console.WriteLine("no of players are{0}", noOfPlayers);
+            Console.WriteLine("country name is {0}", country);
+        }
+        public static DateTime Now { get; }
+    }
+    class Cricket : Game
+    {
+        public static String coachName;
+        public Cricket()
+        {
+
+        }
+        public Cricket(String _country, int _noOfPlayers, String _coachName) :
+            base(_noOfPlayers, _country)
+        {
+            coachName = _coachName;
+
+        }
+        public void DisplayCric(String country, int noOfPlayers, String coachName)
+        {
+    
+            Console.WriteLine("country: {0} no of players:{1} coach Name is:{2}", country, noOfPlayers, coachName);
+        }
+        public void Worldcup()
+        {
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("Australia have won Cricket Worldcup:" + dt.Year);
+        }
+
+    }
+    class FootBall : Game
+    {
+        public static string LeageName;
+        public FootBall()
+        {
+
+        }
+        public FootBall(String _country, int _noOfPlayers, String _LeageName) : base(_noOfPlayers, _country)
+        {
+            LeageName = _LeageName;
+        }
+        public void DisplayFootB(String country, int noOfPlayers, String LeageName)
+        {
+
+            Console.WriteLine("country: {0} noof players:{1} League Name is:{2}", country, noOfPlayers, LeageName);
+
+
+        }
+        public void Worldcup()
+        {
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("Spain have won Football Worldcup:" + dt.Year);
+        }
+    }
+
+    class AssQue2
+    {
+        static void Main()
+        {
+            Game g = new Game(11, "srajvj");
+            g.Display();
+
+
+            Cricket c = new Cricket();
+            Console.WriteLine("---------CRICKET INFORMATION----------");
+            c.DisplayCric("india", 11, "john write");
+            c.DisplayCric("Austrila", 21, "john Bachanss");
+
+            Console.WriteLine("---------FOOTBALL INFORMATION----------");
+            FootBall f = new FootBall();
+            f.DisplayFootB("spain", 14, "spanish Leage");
+            f.DisplayFootB("England", 13, "English Premiur League");
+
+            Cricket india = new Cricket();
+            Cricket australia = new Cricket();
+
+            FootBall spain = new FootBall();
+            FootBall england = new FootBall();
+
+            australia.Worldcup();
+            spain.Worldcup();
+
+            Console.ReadLine();
+        }
+    }
+}
