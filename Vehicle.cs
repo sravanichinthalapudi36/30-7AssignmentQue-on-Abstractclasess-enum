4f824a4a79ad2c9bcec6d1dc2b28bc186cf19764
sravanichinthalapudi36 <68219520+sravanichinthalapudi36@users.noreply.github.com>
2020-08-04 09:08:13 +0530

    Add files via upload

---
+++ Vehicle.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public enum VehType
+    {
+        car,
+        bus,
+        truck
+    };
+    public enum FuelType
+    {
+        diesel,
+        petrol,
+        cng
+    };
+    public class Vehicle
+    {
+        string name;
+        string color;
+        int noOfWheels;
+        VehType vehType;
+        FuelType fuel;
+
+
+
+
+
+        public void Start(VehType vehicletype)
+        {
+            Console.WriteLine(vehicletype + " has started");
+        }
+        public void Stop(VehType vehicletype)
+        {
+            Console.WriteLine(vehicletype + "has stopped");
+        }
+        public Vehicle(string name, string color, VehType veh, int noOfWheels, FuelType fuel)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.NoOfWheel = noOfWheels;
+            this.VehTypes = veh;
+            this.FuelTypes = fuel;
+
+
+
+
+        }
+        public Vehicle(string name, string color, VehType veh, int noOfWheels)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.NoOfWheel = noOfWheels;
+            this.VehTypes = veh;
+            this.FuelTypes = FuelType.petrol;
+        }
+        public Vehicle(string name, string color)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.VehTypes = VehType.car;
+            this.NoOfWheel = 4;
+            this.FuelTypes = FuelType.petrol;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+        public int NoOfWheel
+        {
+            get { return this.noOfWheels; }
+            set { this.noOfWheels = value; }
+        }
+
+
+
+        public VehType VehTypes
+        {
+            get { return this.vehType; }
+            set { this.vehType = value; }
+        }
+        public FuelType FuelTypes
+        {
+            get { return this.fuel; }
+            set { this.fuel = value; }
+        }
+
+
+
+
+        public static void Main()
+        {
+
+
+
+        }
+    }
+}
---
+++ VehicleExe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sample;
+
+namespace VehicleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Vehicle("Cielo", "red");
+            car.Start(car.VehTypes);
+            //car.Stop(car.VehTypes);
+
+
+
+            Vehicle truck = new Vehicle("Tata", "blue", VehType.truck, 6, FuelType.diesel);
+            truck.Start(truck.VehTypes);
+            //truck.Stop(truck.VehTypes);
+
+
+
+            Vehicle smallcar = new Vehicle("Indica", "silver", VehType.car, 4);
+            smallcar.Start(smallcar.VehTypes);
+            //smallcar.Stop(smallcar.VehTypes);
+        }
+    }
+}
